85f8248b37c87ba9e5e33c635d5fd13e173d987f
Paks <promise.chukwudi@yahoo.com>
2023-11-01 14:28:16 +0100

    updated

--- src/Pages/Guest/Guest.tsx
+++ src/Pages/Guest/Guest.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, FormEvent, useState } from "react";
 import "./Guest.scss";
 import Navbar from "../../Components/Navbar/Navbar";
 import Dashboard from "../../Components/Dashboard/Dashboard";
+import Aside from "../../Components/Aside/Aside";
 
 const Guest = () => {
   const [showInfo, setShowInfo] = useState<boolean>(false);
@@ -34,79 +35,125 @@ const Guest = () => {
     ) {
       setShowInfo(false);
     } else {
-      setShowInfo(true)
+      setShowInfo(true);
     }
-
   };
 
   return (
     <div className="guestPage">
       <Navbar />
       <Dashboard />
+      <div className="guestContainer">
+        <div className="guestInfo">
+          {showInfo ? (
+            <div className="guestDisplayInfo">
+              <p>
+                Name: <span>{guestInfo.name}</span>
+              </p>
+              <p>
+                Email: <span>{guestInfo.email}</span>
+              </p>
+              <p>
+                Phone: <span>{guestInfo.phone}</span>
+              </p>
+              <p>
+                Property Type: <span>{guestInfo.type}</span>
+              </p>
+            </div>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="name">Enter Your Full Name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Name"
+                name="name"
+                value={guestInfo.name}
+                onChange={handleChange}
+              />
 
-      <div className="guestInfo">
-        {showInfo ? (
-          <div className="guestDisplayInfo">
-            <p>
-              Name: <span>{guestInfo.name}</span>
-            </p>
-            <p>
-              Email: <span>{guestInfo.email}</span>
-            </p>
-            <p>
-              Phone: <span>{guestInfo.phone}</span>
-            </p>
-            <p>
-              Property Type: <span>{guestInfo.type}</span>
-            </p>
+              <label htmlFor="email">Enter Your Email</label>
+              <input
+                type="text"
+                id="email"
+                placeholder="Email"
+                name="email"
+                value={guestInfo.email}
+                onChange={handleChange}
+              />
+
+              <label htmlFor="name">Enter Your Phone Number</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Phone"
+                name="phone"
+                value={guestInfo.phone}
+                onChange={handleChange}
+              />
+
+              <label htmlFor="name">
+                What Property type are you looking for?
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Property Type i.e For Rent"
+                name="type"
+                value={guestInfo.type}
+                onChange={handleChange}
+              />
+
+              <button>Submit</button>
+            </form>
+          )}
+
+          <div className="gitop">
+            <h1 className="heading">Get in touch</h1>
+            <div className="info">
+              <img
+                src="https://img.icons8.com/ios-filled/100/ffffff/apple-phone.png"
+                alt="apple-phone"
+              />
+              <p>Hotline: +234 81234567</p>
+            </div>
+            <div className="info">
+              <img
+                src="https://img.icons8.com/material-rounded/48/ffffff/mail.png"
+                alt="mail"
+              />
+              <p>Email: info@pakshomes.com</p>
+            </div>
+            <div className="info">
+              <img
+                src="https://img.icons8.com/material-sharp/24/ffffff/marker.png"
+                alt="marker"
+              />
+              <p>Bannex Plaza, FCT, Abuja</p>
+            </div>
+
+            <div className="socials">
+              <img
+                src="https://img.icons8.com/ios-filled/100/ffffff/facebook-f.png"
+                alt="facebook-f"
+              />
+              <img
+                src="https://img.icons8.com/ios/100/ffffff/instagram-new--v1.png"
+                alt="instagram-new--v1"
+              />
+              <img
+                src="https://img.icons8.com/material-rounded/48/ffffff/twitter.png"
+                alt="twitter"
+              />
+              <img
+                src="https://img.icons8.com/ios-filled/100/ffffff/youtube-play.png"
+                alt="youtube-play"
+              />
+            </div>
           </div>
-        ) : (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Enter Your Full Name</label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Name"
-              name="name"
-              value={guestInfo.name}
-              onChange={handleChange}
-            />
+        </div>
 
-            <label htmlFor="email">Enter Your Email</label>
-            <input
-              type="text"
-              id="email"
-              placeholder="Email"
-              name="email"
-              value={guestInfo.email}
-              onChange={handleChange}
-            />
-
-            <label htmlFor="name">Enter Your Phone Number</label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Phone"
-              name="phone"
-              value={guestInfo.phone}
-              onChange={handleChange}
-            />
-
-            <label htmlFor="name">
-              What Property type are you looking for?
-            </label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Property Type i.e For Rent"
-              name="type"
-              value={guestInfo.type}
-              onChange={handleChange}
-            />
-
-            <button>Submit</button>
-          </form>
-        )}
+        <Aside />
       </div>
     </div>
   );
